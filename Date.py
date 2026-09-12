5c52868985b90ead8f9edc704259cc78ccccbe30
vedang2145 <vedang2145@gmail.com>
2026-09-12 12:37:49 +0530

    ADD

---
+++ Date.py
@@ -0,0 +1,19 @@
+Date = int(input("Enter The Number from 1 to 7 : "))
+
+if(Date==1):
+    print("Monday")
+elif(Date==2): 
+    print("Tuesday")
+elif(Date==3): 
+    print("Wednesday")
+elif(Date==4):
+    print("Thursday")
+elif(Date==5):
+    print("Friday")
+elif(Date==6):
+    print("Saturday")
+elif(Date==7):
+    print("Sunday")
+
+else:
+    print("Invalid Number")                          
